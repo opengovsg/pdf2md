97379a4b81950d722adb54c0478fdd0fb4398738
ricktjwong <rickwtj@gmail.com>
2019-06-27 17:51:21 +0800

    Invoke warning for blocktype

--- src/javascript/pdf2md-cli.js
+++ src/javascript/pdf2md-cli.js
@@ -69,7 +69,6 @@ function createMarkdownFiles(filePaths, allOutputPaths) {
   // If outputPath specified, supply callbacks to log progress
   filePaths.forEach(async function(filePath, i) {
       const callbacks = allOutputPaths[i] && {}
-      console.log(filePath)
       const pdfBuffer = fs.readFileSync(filePath)
       pdf2md(pdfBuffer, callbacks)
         .then(text => {

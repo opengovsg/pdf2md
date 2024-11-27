b4199980e3ce6a8090f4b685203e9f7730b774c3
LoneRifle <alwyn.tan@gmail.com>
2024-11-27 05:55:44 +0000

    fix: reinstate pdfjs.Util.transform

--- test/convert.spec.js
+++ test/convert.spec.js
@@ -3,14 +3,13 @@ const fs = require('fs')
 const pdf2md = require('../')
 const path = require('path')
 
-describe('convert pdf to markdown', () => {
+describe('integration test: convert pdf to markdown', () => {
   it('should convert the pdf to markdown', async () => {
     const filePath = path.resolve(__dirname, '../examples/ExamplePdf.pdf')
 
     const pdfBuffer = new Uint8Array(fs.readFileSync(filePath))
     const text = await pdf2md(pdfBuffer)
     fs.writeFileSync('./test/output.md', text)
-    console.log(text)
 
     expect(text.includes(" ")).to.equal(true)
   })
